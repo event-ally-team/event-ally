ea07491ffbfbbbfd09a5f02aa71b9e2f378df042
Stavros Panagiotopoulos <stavros_pana@hotmail.com>
2023-10-19 19:05:32 -0400

    created routes (WIP)

---
+++ controllers/api/commentRoutes.js
@@ -0,0 +1,45 @@
+const router = require('express').Router();
+const {Comments} = require('../../models');
+const withAuth = require('../../utils/auth');
+
+// The `/api/comments` endpoint
+
+router.post('/', async (req, res) => {
+
+    try {
+        const commentData = await Comments.create(req.body);
+        res.status(200).json(commentData);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+    }
+);
+
+router.get('/', async (req, res) => {
+    try {
+        const commentData = await Comments.findAll();
+        res.status(200).json(commentData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+
+    try {
+        const commentData = await Comments.destroy({
+            where: {
+                id: req.params.id,
+            },
+        });
+        if (!commentData) {
+            res.status(404).json({ message: 'No comment found with this id!' });
+            return;
+        }
+        res.status(200).json(commentData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
