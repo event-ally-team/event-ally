19d314f1a0fd358b6735a5abd1bd018feea7b239
event-ally-team <148160099+event-ally-team@users.noreply.github.com>
2023-10-20 18:20:32 +0000

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,6 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proces
 });
 
 sequelize.authenticate().then(() => {console.log('Connection has been established!');})
-  .catch((err) => {console.error('Cannot connect to the DB', err);});
+  .catch((err) => {console.error('Cannot connect to the DB :(', err);});
 
 module.exports = sequelize; 
-
