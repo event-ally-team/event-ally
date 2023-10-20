d82e1cc1c8916e9e3d6b174c7bd111edd89a09f1
event-ally-team <148160099+event-ally-team@users.noreply.github.com>
2023-10-20 17:55:21 +0000

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,14 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-let sequelize;
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: 'localhost',
+  dialect: 'mysql',
+  port: 3306,
+});
 
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306
-    }
-  );
-}
+sequelize.authenticate().then(() => {console.log('Connection has been established!');})
+  .catch((err) => {console.error('Cannot connect to the DB', err);});
 
-module.exports = sequelize;
+module.exports = sequelize; 
+
