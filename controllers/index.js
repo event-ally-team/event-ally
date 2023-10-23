e871affd364c471684525db47e3c13b726f3f3d5
Shaduruk <jide@tidjani-serpos.com>
2023-10-22 23:58:05 -0400

    start fixing api routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 const apiRoutes = require ('./api');
+const signInRoutes = require ('./signInRoutes');
 
-
+router.use('/', signInRoutes);
 router.use('/api', apiRoutes);
 
 module.exports = router;
