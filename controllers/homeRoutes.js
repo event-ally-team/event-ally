a19a8abeeb5ffd985bae2502baccb92f1ff2d0a8
Stavros Panagiotopoulos <stavros_pana@hotmail.com>
2023-10-25 19:22:51 -0400

    linked homeroutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,35 +2,76 @@ const router = require('express').Router();
 const { User } = require('../models');
 const withAuth = require('../utils/auth');
 
-router.post('/signup', async (req, res) => {
-  try {
-    const userData = await User.create(req.body);
+router.get('/', async (req, res) => {
+try {
+  const userData = await User.findAll({
+    include: [
+      {
+        model: User,
+        attributes: ['email'],
+      },
+    ],
+});
 
-    req.session.save(() => {
-      req.session.user_id = userData.id;
-      req.session.logged_in = true;
+const users = userData.map((project) => project.get({ plain: true }));
 
-      res.redirect('/dashboard');
-    });
+res.render('signIn', {
+  users,
+  logged_in: req.session.logged_in,
+});
+} catch (err) {
+res.status(500).json(err);
+}
+});
+
+
+
+router.get('/event', async (req, res) => {
+  try{
+    const eventData = await Event.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+  });
+
+  const events = eventData.map((project) => project.get({ plain: true }));
+
+  res.render('event', {
+    ...events,
+    logged_in: req.session.logged_in,
+  });
   } catch (err) {
-    res.status(400).json(err);
+res.status(500).json(err);
   }
 });
 
-router.get('/login', async (req, res) => {
+router.get('/dashboard', withAuth, async (req, res) => {
+  try {
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Event }],
+  });
+  
+   const user = userData.get({ plain: true });
+
+  res.render('dashboard', {
+    ...user,
+    logged_in: true
+  });
+} catch (err) {
+  res.status(500).json(err);
+}
+});
+
+router.get('/signIn', (req, res) => {
   if (req.session.logged_in) {
-    res.redirect('/signIn');
-
+    res.redirect('/dashboard');
     return;
-
-  } else {
-    
-    try {
-      res.render('signIn');
-    } catch (err) {
-      res.status(500).json(err);
-    }
   }
+  res.render('signIn');
 });
 
 module.exports = router;
