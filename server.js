0b957b42af6f15ef00c1af6e948c880b0af7fca1
Shaduruk <jide@tidjani-serpos.com>
2023-10-26 15:13:30 -0400

    restore server.js file

--- server.js
+++ server.js
@@ -1,79 +1,38 @@
-const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
-const bcrypt = require('bcrypt');
-const sequelize = require('./config/connection');
+
+const sequelize = require('./config/connection.js');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const hbs = exphbs.create({ helpers });
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
+const hbs = exphbs.create({helpers});
 
-app.use(
-  session({
-    secret: 'Super secret secret',
-    cookie: {},
-    resave: false,
-    saveUninitialized: true,
-    store: new SequelizeStore({ db: sequelize }),
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
   })
-);
-
-// Define the User model and make sure to import it
-const User = require('./models/User'); // Import your User model
-
-sequelize.sync();
-
-// Middleware to check if the user is authenticated
-const withAuth = (req, res, next) => {
-  if (!req.session.logged_in) {
-    res.redirect('/login');
-  } else {
-    next();
-  }
 };
 
-app.get('/', (req, res) => {
-  res.render('newEvent.handlebars');
-});
+app.use(session(sess));
 
-app.get('/login', (req, res) => {
-  res.render('login');
-});
-
-app.post('/login', async (req, res) => {
-  const { email, password } = req.body;
-  const user = await User.findOne({ where: { email } });
-
-  if (user && bcrypt.compareSync(password, user.password)) {
-    req.session.logged_in = true;
-    req.session.user = user;
-    res.redirect('/');
-  } else {
-    res.render('login', { error: 'Invalid email or password' });
-  }
-});
-
-app.get('/logout', (req, res) => {
-  req.session.destroy(() => {
-    res.redirect('/');
-  });
-});
-
-app.get('/protected-route', withAuth, (req, res) => {
-  res.send('This is a protected route');
-});
-
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'));
 app.use(routes);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`); });
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
+
