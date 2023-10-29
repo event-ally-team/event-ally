2362897befeecddbcade5e7c90f94cda963c99da
Shaduruk <jide@tidjani-serpos.com>
2023-10-29 09:50:03 -0400

    feature: populate new events with generic event items based on their type

--- public/assets/js/newEvent.js
+++ public/assets/js/newEvent.js
@@ -14,7 +14,7 @@
 
 
 //  if (eventTitle.value.trim() && categorySelector.value.trim() && eventStartDate.value.trim() && eventEndDate.value.trim()) {
-    const response = await fetch(`/api/events/`, {
+    const response1 = await fetch(`/api/events/`, {
       method: 'POST',
       body: JSON.stringify({ title, type, start_date, end_date }),
       headers: {
@@ -22,16 +22,40 @@
       },
     });
 
-    if (response.ok) {
+    if (response1.ok) {
+
+      const response2 = await fetch(`/api/defaulteventitems/${response1.type}`, {
+        method: 'GET',
+      });
+
+      if (response2.ok) {
+
+        const seedDatabase = async () => {
+
+      for (const eventItem of response2) {
+          await EventItem.create({
+            ...eventItem,
+            event_id: response1.event_id,
+            user_id: req.session.user_id,
+          });
+        }
+
+        process.exit(0);
+      };
+
+      seedDatabase();
+
+      } else {
+        alert('Failed to load generic event item data');
+      }
+
       document.location.replace('/dashboard');
     } else {
       alert('Failed to create event');
     }
-  
 
 };
 
-  
   createEventBtn.addEventListener("click", () => {
     newFormHandler()
   });
